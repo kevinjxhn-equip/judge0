4d4ee76a67fe16d0df6aedc0400c9e3eb587f59c
Kevin <kevin@equip.co>
2024-03-12 12:39:23 +0530

    checking the path

--- judge0-monaco/server/server.js
+++ judge0-monaco/server/server.js
@@ -27,10 +27,10 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../index.html")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 // Judge0 API instance
